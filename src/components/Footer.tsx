e4c6867b89c1385f3b73e8269215de76e307f38f
Rikard Johansson <Rikard.com@hotmail.com>
2023-02-13 19:38:00 +0100

    Added Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,34 @@
+import Link from "next/link";
 import React, { FC } from "react";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { IconButton } from "@mui/material";
 
 interface FooterProps {}
 
-const navigation = [{}];
+const navigation = [
+  {
+    icon: <GitHubIcon />,
+    href: "https://github.com/Rikard-Johansson97",
+  },
+  {
+    icon: <LinkedInIcon />,
+    href: "https://www.linkedin.com/in/rikard-johansson-571844249/",
+  },
+];
 
 const Footer: FC<FooterProps> = ({}) => {
   return (
-    <div className='flex bg-black'>
+    <div className='flex bg-[url("https://www.transparenttextures.com/patterns/cartographer.png")] bg-background border-t-2 border-greenText justify-center'>
       <nav>
-        <Link></Link>
+        {navigation.map((link, i) => (
+          <IconButton
+            key={i}
+            className='text-greenText transition-colors duration-300 hover:text-highlight'>
+            <Link href={link.href} target='_blank'></Link>
+            {link.icon}
+          </IconButton>
+        ))}
       </nav>
     </div>
   );
