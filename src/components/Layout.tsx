8a3299e824999f2c70bbcb03a76d7c4a9544439e
Rikard Johansson <Rikard.com@hotmail.com>
2023-02-21 20:07:24 +0100

    Fixed Background

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,7 +9,7 @@ interface LayoutProps {
 
 const Layout: FC<LayoutProps> = ({ children }) => {
   return (
-    <div className=' min-h-screen min-w-full bg-lightBackground flex flex-col'>
+    <div className=' min-h-screen min-w-full bg-background flex flex-col'>
       <main className='flex-1'>{children}</main>
 
       <Footer />
