258dda62baafb03768aaf7228feb392595fe8e89
Rikard Johansson <106700975+Rikard-Johansson97@users.noreply.github.com>
2023-03-06 17:30:12 +0100

    Revert "Remove services"
    This reverts commit bdb8c59a703244daa0ecb709f8fd82b26d8f2802.

---
+++ src/components/Services.tsx
@@ -0,0 +1,79 @@
+import Image from "next/image";
+import React, { FC } from "react";
+import CodeIcon from "@mui/icons-material/Code";
+import DesignServicesIcon from "@mui/icons-material/DesignServices";
+import SettingsSuggestIcon from "@mui/icons-material/SettingsSuggest";
+import { useSessionStorage } from "usehooks-ts";
+import FadeIn from "./FadeIn";
+
+type ServiceType = {
+  title: string;
+  desc: string;
+  icon: JSX.Element;
+};
+
+type ServicesType = {
+  en: ServiceType[];
+  se: ServiceType[];
+};
+
+const services: ServicesType = {
+  en: [
+    {
+      title: "Web Development",
+      desc: "Let me help you stand out online with custom landing pages that convert and web development services that drive results.",
+      icon: <CodeIcon fontSize='large' />,
+    },
+    {
+      title: "UX / UI Design",
+      desc: "Elevate your brand and engage your audience with my custom UX/UI design services. Let's enhance your website's user experience together!",
+      icon: <DesignServicesIcon fontSize='large' />,
+    },
+    {
+      title: "SEO optimization",
+      desc: "Boost your online visibility & grow your business with my SEO services! Optimize your website for search engines & drive more traffic.",
+      icon: <SettingsSuggestIcon fontSize='large' />,
+    },
+  ],
+  se: [
+    {
+      title: "Webbutveckling",
+      desc: "Skapar anpassade landningssidor som får kunderna att handla - låt mig hjälpa dig att sticka ut online!",
+      icon: <CodeIcon fontSize='large' />,
+    },
+    {
+      title: "UX / UI Design",
+      desc: "Med mina anpassade UX/UI design-tjänster kan jag hjälpa dig att höja din varumärkesprofil och engagera din publik på din webbplats",
+      icon: <DesignServicesIcon fontSize='large' />,
+    },
+    {
+      title: "SEO-optimering",
+      desc: "Få synlighet på nätet med mina SEO-tjänster! Jag optimerar din webbplats för sökmotorer och drar mer trafik till din sida.",
+      icon: <SettingsSuggestIcon fontSize='large' />,
+    },
+  ],
+};
+
+const Services: FC = ({}) => {
+  const [language, setLanguage] = useSessionStorage("lang", "en");
+
+  return (
+    <FadeIn>
+      <div className='grid grid-cols-auto-fit-300 p-4 gap-4 w-full max-w-5xl mx-auto'>
+        {services[language as keyof typeof services]?.map((service, i) => (
+          <div
+            key={i}
+            className='flex flex-col justify-center text-headline sm:max-w-xs mx-auto flex-wrap px-4 py-10 bg-lightBackground rounded-md shadow'>
+            <div className='text-center flex-1'>
+              {service.icon}
+              <h3 className='text-xl font-bold mt-4 mb-2'>{service.title}</h3>
+              <p className='text-paragraph'>{service.desc}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </FadeIn>
+  );
+};
+
+export default Services;
